0b0cc6b099312578e29d3e9d80b577e5c1f98ca3
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-08 03:15:24 +0800

    Added bottom bar

--- App.js
+++ App.js
@@ -3,10 +3,17 @@ import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
+
+import Colors from "./src/constants/colors";
+import Dimens from "./src/constants/dimens";
 
 import HomeScreen from './src/screens/HomeScreen';
 import ScanScreen from './src/screens/ScanScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
+import InterText from './src/components/InterText';
 
 const fetchFonts = () => {
     return Font.loadAsync({
@@ -18,6 +25,54 @@ const fetchFonts = () => {
 };
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const screenOptions = ({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        switch (route.name) {
+            case "Home":
+                iconName = "format-list-bulleted";
+                break;
+            case "Scan":
+                iconName = "filter-center-focus";
+                break;
+            case "Settings":
+                iconName = "settings";
+                break;
+        }
+
+        return <Icon name={iconName} size={size} color={color} />;
+    },
+
+    tabBarLabel: ({ focused, color }) => {
+        let label;
+
+        switch (route.name) {
+            case "Home":
+                label = "Locations";
+                break;
+            case "Scan":
+                label = "Scan";
+                break;
+            case "Settings":
+                label = "Settings";
+                break;
+        }
+
+        return <InterText flavor="medium" color={color} size={12}>{label}</InterText>;
+    }
+});
+
+const tabBarOptions = {
+    activeTintColor: Colors.primaryLighter,
+    inactiveTintColor: Colors.grey3,
+    keyboardHidesTabBar: true,
+    style: {
+        elevation: 100
+    }
+};
 
 export default function App() {
     const [assetsLoaded, setAssetsLoaded] = useState(false);
@@ -29,11 +84,15 @@ export default function App() {
 
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" headerMode="none">
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Scan" component={ScanScreen} />
-                <Stack.Screen name="Settings" component={SettingsScreen} />
-            </Stack.Navigator>
+            <Tab.Navigator
+                screenOptions={screenOptions}
+                tabBarOptions={tabBarOptions}
+                backBehavior="none"
+            >
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Scan" component={ScanScreen} />
+                <Tab.Screen name="Settings" component={SettingsScreen} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
