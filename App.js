8b807711d1159153dc97f53c396e2ebfcb22943e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-08 02:39:13 +0800

    Added ScanScreen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from './src/screens/HomeScreen';
+import ScanScreen from './src/screens/HomeScreen';
 
 const fetchFonts = () => {
     return Font.loadAsync({
@@ -29,6 +30,7 @@ export default function App() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home" headerMode="none">
                 <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Scan" component={ScanScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
---
+++ src/screens/ScanScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View, SectionList } from "react-native";
+
+import Colors from "../constants/colors";
+import Dimens from "../constants/dimens";
+import Strings from "../constants/strings";
+
+export default (props) => {
+    return (
+        <View style={styles.screen}>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: Colors.grey1
+    }
+});
