1108dce126f96f04720d7f4c7b6ea8cd480947b3
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-08 15:43:03 +0800

    Removed "URL" from SafeEntry menu caption

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -5,7 +5,7 @@ export default {
     checkedInPlaces: "Checked in places",
     lastVisited: "Last visited",
     yourLocations: "Your locations",
-    openURL: "Open SafeEntry URL",
+    openURL: "Open SafeEntry",
     rescanQR: "Rescan QR code",
     rename: "Rename",
     deleteLocation: "Delete location"
