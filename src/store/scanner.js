cf1dd800e1addade80d35255f0b2c28be20dcb4f
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-09 03:05:58 +0800

    Added scanner reducers

--- src/store/scanner.js
+++ src/store/scanner.js
@@ -1,13 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
 
 const slice = createSlice({
-    name: "settings",
+    name: "scanner",
     initialState: false,
     reducers: {
         scannerScanned: (scanned, action) => {
             scanned = true;
+        },
+
+        scannerEnabled: (scanned, action) => {
+            scanned = false;
         }
     }
 });
 
+const {
+    scannerScanned,
+    scannerEnabled
+} = slice.actions;
+
 export default slice.reducer;
+
+export const enableScanning = () => scannerEnabled();
+export const disableScanning = () => scannerScanned();
+
+export const getScannerStatus = createSelector(
+    state => state.scanner,
+    scanner => scanner
+);
