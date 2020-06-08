f7300a6c33c710d62458042f9585eb70151607dd
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-09 01:05:09 +0800

    Added list to SettingsScreen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,16 +1,82 @@
-import React from 'react';
-import { StyleSheet, View, SectionList } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, View, SectionList, TouchableHighlight } from "react-native";
+import { useDispatch } from "react-redux";
 
 import Colors from "../constants/colors";
 import Dimens from "../constants/dimens";
 import Strings from "../constants/strings";
 
 import Header from "../components/Header";
+import InterText from "../components/InterText";
+import Prompt from "../components/Prompt";
+
+import {
+    resetLocations
+} from "../store/locations";
+
+const renderListItem = ({ item, index, section, separators }) => (
+    <TouchableHighlight activeOpacity={0.95} underlayColor={Colors.black} onPress={item.onPress}><View style={styles.menuItem}>
+        <InterText size={17}>{item.label}</InterText>
+    </View></TouchableHighlight>
+);
+
+const renderListSectionHeader = ({ section: { title } }) => (
+    <InterText flavor="semibold" size={15} style={styles.listSectionHeader}>{title}</InterText>
+);
 
 export default (props) => {
+    const [deleteLocationsPromptVisible, setDeleteLocationsPromptVisible] = useState(false);
+
+    const settings = [
+        {
+            title: "Satu",
+            data: [
+                {
+                    id: "resetLocations",
+                    label: "Erase all locations",
+                    onPress: () => setDeleteLocationsPromptVisible(true)
+                },
+                {
+                    id: "resetApp",
+                    label: "Reset application state",
+                    onPress: () => alert("helo")
+                },
+            ]
+        }
+    ];
+
     return (
         <View style={styles.screen}>
             <Header title="Settings" />
+
+            <Prompt
+                visible={deleteLocationsPromptVisible}
+                dismissMe={() => setDeleteLocationsPromptVisible(false)}
+                title="Are you sure?"
+                message="Yeaboi"
+                onYes={() => alert("Yes")}
+                onNo={() => alert("No")}
+                yesCaption="Yes"
+                noCaption="No"
+            />
+
+            <View style={styles.container}>
+                <SectionList
+                    sections={settings}
+                    renderItem={renderListItem}
+                    renderSectionHeader={renderListSectionHeader}
+                    ItemSeparatorComponent={
+                        ({ highlighted, leadingItem }) => (
+                            <View style={styles.menuItemSeparator}>
+                                <View style={{backgroundColor: Colors.white, width: 30, height: 1}} />
+                            </View>
+                        )
+                    }
+                    ListFooterComponent={
+                        <View style={{height: 100}} />
+                    }
+                />
+            </View>
         </View>
     );
 };
@@ -18,6 +84,29 @@ export default (props) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
+        backgroundColor: Colors.grey1
+    },
+
+    container: {
+        flex: 1
+    },
+
+    menuItem: {
+        height: 50,
+        backgroundColor: Colors.white,
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 30
+    },
+
+    listSectionHeader: {
+        marginTop: 15,
+        marginBottom: 10,
+        marginHorizontal: 30
+    },
+
+    menuItemSeparator: {
+        height: 1,
         backgroundColor: Colors.grey1
     }
 });
