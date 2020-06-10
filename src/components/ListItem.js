49eec55d45e19915139309a04d03394313a7aa92
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-10 16:37:48 +0800

    Added last visited to Checked in

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -16,9 +16,9 @@ export default (props) => {
 
     const renderSubcaption = () => {
         if (checkedIn) {
-            return <InterText color={Colors.secondaryLighter} size={15} numberOfLines={1}>{Strings.checkedIn}</InterText>
+            return <InterText color={Colors.secondaryLighter} size={15} numberOfLines={1}>{Strings.checkedIn} {moment(lastVisited).format("ddd MMM D, YYYY, H:m")}</InterText>
         } else {
-            return <InterText color={Colors.grey3} size={15} numberOfLines={1}>{`${Strings.lastVisited} ${moment(lastVisited).format("ddd MMM D, YYYY, H:m")}`}</InterText>
+            return <InterText color={Colors.grey3} size={15} numberOfLines={1}>{Strings.lastVisited} {moment(lastVisited).format("ddd MMM D, YYYY, H:m")}</InterText>
         }
     };
     
